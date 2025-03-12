e23caf855f984ded57320122f9da27e0d842c388
1ithiA <fon.patcha@gmail.com>
2025-03-12 18:18:06 +0700

    update add migration

---
+++ ConsoleApp1/WebApp_Fon/Data/DataContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebApp_Fon.Model;
+
+namespace TestDb1.Data;
+
+public class DataContext : DbContext
+{
+    private string DbPath { get; }
+
+    public DbSet<Vehicle> Vehicles { get; set; }
+
+    public DataContext()
+    {
+        var folder = "DB";
+        var path = folder;
+        Console.WriteLine($"Path: {path}");
+        DbPath = System.IO.Path.Join(path, "Data.db");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    => options.UseSqlite($"Data Source={DbPath}");
+}
+
+
---
+++ ConsoleApp1/WebApp_Fon/Model/Vehicle.cs
@@ -0,0 +1,9 @@
+namespace WebApp_Fon.Model;
+
+public class Vehicle
+{
+    public Guid Id { get; set; }
+    public string? Brand { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    
+}
