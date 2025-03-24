3d256224dfd82028cf4f4627c1a1972f79374251
Flintster <beginflints@gmail.com>
2025-03-24 19:11:02 +0700

    Tone Push

---
+++ ConsoleApp1/TestDb1/ModelDtos/VehicleDto.cs
@@ -0,0 +1,7 @@
+namespace TestDb1.ModelDtos;
+
+public class VehicleDto
+{
+    public string? Brand { get; set; }
+    public DateTime ReleasedDate { get; set; }
+}
---
+++ ConsoleApp1/TestDb1/Models/Vehicle.cs
@@ -0,0 +1,8 @@
+namespace TestDb1.Models;
+
+public class Vehicle
+{
+    public Guid Id { get; set; }
+    public string? Brand { get; set; }
+    public DateTime ReleasedDate { get; set; }
+}
