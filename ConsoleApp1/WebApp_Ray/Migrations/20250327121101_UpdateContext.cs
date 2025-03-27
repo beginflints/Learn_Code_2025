056a6f0f2b054ca718ad4176e81a58bb386ff570
Ray <156043273+RawatArt@users.noreply.github.com>
2025-03-27 19:13:23 +0700

    Update DataRay

---
+++ ConsoleApp1/WebApp_Ray/Migrations/20250327121101_UpdateContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp_Ray.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RoofColor",
+                table: "Vehicles",
+                newName: "Speed");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Vehicles",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Vehicles");
+
+            migrationBuilder.RenameColumn(
+                name: "Speed",
+                table: "Vehicles",
+                newName: "RoofColor");
+        }
+    }
+}
