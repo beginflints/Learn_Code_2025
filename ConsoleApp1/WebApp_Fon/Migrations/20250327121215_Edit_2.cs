78f792589960162099ca6c8dc82212a1d36ebf21
1ithiA <fon.patcha@gmail.com>
2025-03-27 19:13:17 +0700

    Migrated and updated database

---
+++ ConsoleApp1/WebApp_Fon/Migrations/20250327121215_Edit_2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp_Fon.Migrations
+{
+    /// <inheritdoc />
+    public partial class Edit_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReleaseDate",
+                table: "Vehicles",
+                newName: "ReleasedDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Vehicles",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Speed",
+                table: "Vehicles",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "Speed",
+                table: "Vehicles");
+
+            migrationBuilder.RenameColumn(
+                name: "ReleasedDate",
+                table: "Vehicles",
+                newName: "ReleaseDate");
+        }
+    }
+}
