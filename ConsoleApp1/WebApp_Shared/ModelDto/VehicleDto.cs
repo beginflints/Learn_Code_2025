849f0a0fb755cb81132800eaf2bbe0412a584143
Flintster <beginflints@gmail.com>
2025-03-27 18:45:44 +0700

    Update VehicleDto

--- ConsoleApp1/WebApp_Shared/ModelDto/VehicleDto.cs
+++ ConsoleApp1/WebApp_Shared/ModelDto/VehicleDto.cs
@@ -2,7 +2,8 @@ namespace WebApp_Shared.ModelDto;
 
 public class VehicleDto
 {
-    
     public string? Brand { get; set; }
     public DateTime ReleasedDate { get; set; }
+    public string? Engine { get; set; }
+    public string? Model { get; set; }
 }
